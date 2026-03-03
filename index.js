79474f0bf0bafe4b036507b2f16dac761831ca91
jayanth <workstuffjayanth@gmail.com>
2026-03-03 13:28:35 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const http = require("http");
 const fs = require("fs");
-const {sockets} = require("socket.io");
+const { Server } = require("socket.io")
+
+let va = "/.about";
+
+// all about servers 
 
 const server = http.createServer((req, res) => {
 
-    fs.readFile("/home/jayanth/dev/js-project/scrible-clone/public/index.html", (err, data) => {
+    if(req.url == "/"){
+
+    fs.readFile("/home/jayanth/dev/js-project/scrible.com/public/index.html", (err, data) => {
 
         if (err) {
             res.writeHead(404, { "Content-Type": "text/plain"});
@@ -15,11 +21,50 @@ const server = http.createServer((req, res) => {
 
         res.writeHead(404, { "Content-Type": "text/html"});
             res.end(data);
-    })
+    })}
+
+    if(req.url == va) {
+         fs.readFile("/home/jayanth/dev/js-project/scrible.com/user.html", (err, data) => {
+
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/plain"});
+            res.end("there is an error");
+
+
+        }
+
+        res.writeHead(404, { "Content-Type": "text/html"});
+            res.end(data);
+    })}
 
 })
-    
 
+// dealing with sockets and everything related to sockets 
+        let y;
+        const io = new Server(server);
+        
+    
+        io.on("connection", (sockets) => {
+            
+           console.log("my server is up");
+           sockets.on("draw", (x) => {
+            
+             sockets.broadcast.emit("project", x);
+
+            
+           })
+           
+            
+            
+
+           
+        })
+
+
+
+
+
+// all about listen for the port 
         server.listen(8080, () => {
          console.log('Server running at http://localhost:8080');
                 });
